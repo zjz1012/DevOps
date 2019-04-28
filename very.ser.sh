f1e71b5261d000f1dae0f32680f51c60d3c4ba5a
zjz1012 <38127429+zjz1012@users.noreply.github.com>
2019-04-28 22:28:33 +0800

    Update very.ser.sh

--- very.ser.sh
+++ very.ser.sh
@@ -1,3 +1,7 @@
+#只检查服务vsftpd httpd sshd crond、mysql中任意一个服务的状态
+#如果不是这5个中的服务，就提示用户能够检查的服务名并退出脚本
+#如果服务是运行着的就输出 "服务名 is running"
+#如果服务没有运行就启动服务
 #!/bin/bash
 read -p "请输入你的服务名:" service
 if [ $service != 'crond' -a $service != 'httpd' -a $service != 'sshd' -a $service != 'mysqld' -a $service != 'vsftpd' ];then
