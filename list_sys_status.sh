4e397ffee12c806b67de574486470611951396aa
zjz1012 <38127429+zjz1012@users.noreply.github.com>
2019-04-28 22:27:57 +0800

    Update list_sys_status.sh

--- list_sys_status.sh
+++ list_sys_status.sh
@@ -1,3 +1,5 @@
+#显示系统使用的以下信息：
+#主机名、IP地址、子网掩码、网关、DNS服务器IP地址信息
 #!/bin/bash
 IP=`ifconfig eth0 | head -2 | tail -1 | awk '{print $2}' | awk -F":" '{print $2}'`
 ZW=` ifconfig eth0 | head -2 | tail -1 | awk '{print $3}' | awk -F":" '{print $2}'`
